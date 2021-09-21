4b9982386993e630aeeb6c315712bc90ab8494fe
starrytky <starrytky@qq.com>
2021-09-21 23:57:18 +0800

    新增按钮功能，新增对应的localstorage存储

--- src/main.js
+++ src/main.js
@@ -1,53 +1,50 @@
-const $siteList = $('.siteList')
-const $lastLi = $siteList.find('li.last')
-const x = localStorage.getItem('x')
-const xObject = JSON.parse(x)
+const $siteList = $('.siteList');
+const $addLink = $('#addLink');
+const x = localStorage.getItem('x');
+const xObject = JSON.parse(x);
 const hashMap = xObject || [
   {logo: 'A', url: 'https://www.acfun.cn'},
   {logo: 'B', url: 'https://www.bilibili.com'}
 ]
-const simplifyUrl = (url) => {
-  return url.replace('https://', '')
-    .replace('http://', '')
+const deleteUrl = (url) => {
+  return url.replace('http://', '')
+    .replace('https://', '')
     .replace('www.', '')
-    .replace(/\/.*/, '') // 删除 / 开头的内容
+    .replace(/\/.*/, '')
 }
 
 const render = () => {
-  $siteList.find('li:not(.last)').remove()
+  $siteList.find('li:not(#addLink)').remove()
   hashMap.forEach((node, index) => {
     const $li = $(`<li>
-      <div class="site">
-        <div class="logo">${node.logo}</div>
-        <div class="link">${simplifyUrl(node.url)}</div>
-        <div class="close">
-          <svg class="icon">
-            <use xlink:href="#icon-close"></use>
-          </svg>
-        </div>
-      </div>
-    </li>`).insertBefore($lastLi)
-    $li.on('click', () => {
-      window.open(node.url)
-    })
-    $li.on('click', '.close', (e) => {
-      e.stopPropagation() // 阻止冒泡
-      hashMap.splice(index, 1)
-      render()
-    })
-  })
-}
+    <div class="title">${node.logo}</div>
+    <div class="link">${deleteUrl(node.url)}</div>
+    <div class="close">
+      <svg class="icon">
+        <use xlink:href="#icon-close"></use>
+      </svg>
+    </div>
+  </li>`).insertBefore($addLink)
 
+  $li.on('click', () => {
+    window.open(node.url)
+  })
+  $li.on('click', '.close', (e) => {
+    e.stopPropagation() // 阻止冒泡
+    hashMap.splice(index, 1)
+    render()
+  })
+  });
+}
 render()
 
-$('.addButton').on('click', () => {
-  let url = window.prompt('请问你要添加的网址是啥？')
-  if (url.indexOf('http') !== 0) {
-    url = 'https://' + url
+$("#addLink").on('click', () => {
+  let url = window.prompt('请输入你要添加的网址')
+  if(url.indexOf('http') !== 0){
+    url = 'https://' + url;
   }
-  console.log(url)
   hashMap.push({
-    logo: simplifyUrl(url)[0].toUpperCase(),
+    logo: deleteUrl(url)[0].toUpperCase(),
     url: url
   })
   render()
